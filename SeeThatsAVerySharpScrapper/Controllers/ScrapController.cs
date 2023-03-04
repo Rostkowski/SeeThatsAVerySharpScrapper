193d4f0575adc60e904d4373118307a3f5a4892e
Karol Rostkowski <krostkowski@giganciprogramowania.edu.pl>
2023-03-04 20:03:23 +0100

    Add project files.

---
+++ SeeThatsAVerySharpScrapper/Controllers/ScrapController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeeThatsAVerySharpScrapper.Models;
+
+namespace SeeThatsAVerySharpScrapper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScrapController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public async Task<IActionResult> ScrapWebsites([FromBody] ScrapParameters scrapParameters)
+        {
+            HttpClient client = new();
+
+            var result = await client.GetStreamAsync(scrapParameters.Url);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ SeeThatsAVerySharpScrapper/Models/ScrapParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace SeeThatsAVerySharpScrapper.Models
+{
+    public class ScrapParameters
+    {
+        public string? Url { get; set; }
+        public List<string>? CssSelectors { get; set; }
+    }
+}
